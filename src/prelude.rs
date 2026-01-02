48986c500b01284edc2a623671da630907aa1fb0
daniel <kaluifesinachi@yahoo.com>
2026-01-02 13:02:06 +0100

    Re-export from core

---
+++ src/prelude.rs
@@ -0,0 +1,11 @@
+﻿//! Prelude for convenient imports
+
+pub use crate::message::{Message, MessageId};
+pub use crate::builder::MessageBuilder;
+pub use crate::performative::Performative;
+pub use crate::router::Router;
+pub use crate::mailbox::Mailbox;
+pub use crate::error::MessagingError;
+
+// Re-export from core
+pub use agentropic_core::prelude::*;
