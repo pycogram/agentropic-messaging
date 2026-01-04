52879c0c59f0e5a95c01566d3607a27d573c1030
daniel <kaluifesinachi@yahoo.com>
2026-01-04 13:01:28 +0100

    Request-Reply protocol helper

---
+++ src/protocols/request_reply.rs
@@ -0,0 +1,15 @@
+﻿/// Request-Reply protocol helper
+pub struct RequestReply;
+
+impl RequestReply {
+    /// Create new instance
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl Default for RequestReply {
+    fn default() -> Self {
+        Self::new()
+    }
+}
