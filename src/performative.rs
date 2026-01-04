edcdd3b77a7eb55e8232d10e54ed19091f1fdc50
daniel <kaluifesinachi@yahoo.com>
2026-01-04 12:59:06 +0100

    Message performative

---
+++ src/performative.rs
@@ -0,0 +1,17 @@
+﻿use serde::{Deserialize, Serialize};
+
+/// Message performative (speech act)
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum Performative {
+    Inform,
+    Request,
+    Query,
+    Propose,
+    Accept,
+    Reject,
+    Confirm,
+    Disconfirm,
+    Subscribe,
+    CFP,
+    Refuse,
+}
