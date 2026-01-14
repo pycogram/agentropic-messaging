56ceb827473f7d861ac030fb7e3bdee4804fa313
daniel <kaluifesinachi@yahoo.com>
2026-01-14 05:42:09 +0100

    Messaging errors

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,17 @@
-﻿use thiserror::Error;
+use thiserror::Error;
 
 /// Messaging errors
 #[derive(Error, Debug)]
 pub enum MessagingError {
-
     #[error("Agent not found")]
     AgentNotFound,
-    
+
     #[error("Send failed: {0}")]
     SendFailed(String),
-    
+
     #[error("Lock error")]
     LockError,
-    
+
     /// Other error
     #[error("Messaging error: {0}")]
     Other(String),
