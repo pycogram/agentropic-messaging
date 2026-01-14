02e82566ba24903e62cbdb2f7e2094bea03c57f1
daniel <kaluifesinachi@yahoo.com>
2026-01-14 05:45:11 +0100

    test message

--- tests/message_tests.rs
+++ tests/message_tests.rs
@@ -1,12 +1,12 @@
-﻿use agentropic_messaging::prelude::*;
+use agentropic_messaging::prelude::*;
 
 #[test]
 fn create_message() {
     let sender = AgentId::new();
     let receiver = AgentId::new();
-    
+
     let msg = Message::new(sender, receiver, Performative::Inform, "test");
-    
+
     assert_eq!(msg.sender(), sender);
     assert_eq!(msg.receiver(), receiver);
     assert_eq!(msg.performative(), Performative::Inform);
@@ -17,14 +17,14 @@ fn create_message() {
 fn message_builder() {
     let sender = AgentId::new();
     let receiver = AgentId::new();
-    
+
     let msg = MessageBuilder::new()
         .sender(sender)
         .receiver(receiver)
         .performative(Performative::Request)
         .content("Hello")
         .build();
-    
+
     assert_eq!(msg.sender(), sender);
     assert_eq!(msg.content(), "Hello");
 }
